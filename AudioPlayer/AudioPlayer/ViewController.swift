d364142e215c29dadf3f606a5791d336b73ec700
Amanzhan Khassen <zharkynuly11@gmail.com>
2022-11-03 14:42:22 +0600

    Created project

---
+++ AudioPlayer/AudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Amanzhan Zharkynuly on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
