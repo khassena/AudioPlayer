e1318a66c1ed715b044e59c4933182278896e3b4
Amanzhan Khassen <zharkynuly11@gmail.com>
2022-11-03 23:44:32 +0600

    Finished creating UI elements and Auto Layout of first screen PlaylistVC

--- AudioPlayer/AudioPlayer/ViewControllers/PlaylistViewController.swift
+++ AudioPlayer/AudioPlayer/ViewControllers/PlaylistViewController.swift
@@ -9,16 +9,27 @@ import UIKit
 
 class PlaylistViewController: UIViewController {
 
-    @IBOutlet weak var songImage: UIImageView!
-    @IBOutlet weak var nameOfSong: UILabel!
-    @IBOutlet weak var durationOfSong: UILabel!
-   
-
+    @IBOutlet weak var songImageFirst: UIImageView!
+    @IBOutlet weak var nameOfSongFirst: UILabel!
+    @IBOutlet weak var durationOfSongFirst: UILabel!
+    @IBOutlet weak var FirstSongStackView: UIStackView!
+    
+    
+    @IBOutlet weak var songImageSecond: UIImageView!
+    @IBOutlet weak var nameOfSongSecond: UILabel!
+    @IBOutlet weak var durationOfSongSecond: UILabel!
+    @IBOutlet weak var secondSongStackView: UIStackView!
+    
+    @IBOutlet weak var songImageThird: UIImageView!
+    @IBOutlet weak var nameOfSongThird: UILabel!
+    @IBOutlet weak var durationOfSongThird: UILabel!
+    @IBOutlet weak var thirdSongStackView: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
-
+   
 }
 
